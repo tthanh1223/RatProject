e80f1e438b0936c4e8747ca1f483a4937d7aa62d
tthanh1223 <lqd.tk23.vothaithanh@gmail.com>
2025-12-05 22:54:55 +0700

    thêm ô nhập liệu

--- ServerApp/WebSocketTest/Form1.Designer.cs
+++ ServerApp/WebSocketTest/Form1.Designer.cs
@@ -2,41 +2,27 @@
 {
     partial class Form1
     {
-        /// <summary>
-        /// Required designer variable.
-        /// </summary>
         private System.ComponentModel.IContainer components = null;
 
-        /// <summary>
-        /// Clean up any resources being used.
-        /// </summary>
-        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
         protected override void Dispose(bool disposing)
         {
-            if (disposing && (components != null))
-            {
-                components.Dispose();
-            }
+            if (disposing && (components != null)) components.Dispose();
             base.Dispose(disposing);
         }
 
-        #region Windows Form Designer generated code
-
-        /// <summary>
-        /// Required method for Designer support - do not modify
-        /// the contents of this method with the code editor.
-        /// </summary>
         private void InitializeComponent()
         {
             this.btnStart = new System.Windows.Forms.Button();
             this.txtLog = new System.Windows.Forms.RichTextBox();
+            this.txtMessage = new System.Windows.Forms.TextBox();
+            this.btnSend = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // btnStart
             // 
             this.btnStart.Location = new System.Drawing.Point(12, 12);
             this.btnStart.Name = "btnStart";
-            this.btnStart.Size = new System.Drawing.Size(120, 40);
+            this.btnStart.Size = new System.Drawing.Size(100, 35);
             this.btnStart.TabIndex = 0;
             this.btnStart.Text = "Start Server";
             this.btnStart.UseVisualStyleBackColor = true;
@@ -46,26 +32,47 @@
             // 
             this.txtLog.Location = new System.Drawing.Point(12, 60);
             this.txtLog.Name = "txtLog";
-            this.txtLog.Size = new System.Drawing.Size(460, 300);
-            this.txtLog.TabIndex = 1;
+            this.txtLog.ReadOnly = true;
+            this.txtLog.Size = new System.Drawing.Size(460, 250);
+            this.txtLog.TabIndex = 3;
             this.txtLog.Text = "";
+            // 
+            // txtMessage
+            // 
+            this.txtMessage.Location = new System.Drawing.Point(12, 325);
+            this.txtMessage.Name = "txtMessage";
+            this.txtMessage.Size = new System.Drawing.Size(350, 22);
+            this.txtMessage.TabIndex = 1;
+            this.txtMessage.KeyDown += new System.Windows.Forms.KeyEventHandler(this.txtMessage_KeyDown);
+            // 
+            // btnSend
+            // 
+            this.btnSend.Location = new System.Drawing.Point(372, 323);
+            this.btnSend.Name = "btnSend";
+            this.btnSend.Size = new System.Drawing.Size(100, 26);
+            this.btnSend.TabIndex = 2;
+            this.btnSend.Text = "Gửi (Chat)";
+            this.btnSend.UseVisualStyleBackColor = true;
+            this.btnSend.Click += new System.EventHandler(this.btnSend_Click);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(484, 381);
+            this.ClientSize = new System.Drawing.Size(484, 361);
+            this.Controls.Add(this.btnSend);
+            this.Controls.Add(this.txtMessage);
             this.Controls.Add(this.txtLog);
             this.Controls.Add(this.btnStart);
             this.Name = "Form1";
-            this.Text = "WebSocket Server WinForms";
+            this.Text = "WebSocket Server (Full Duplex)";
             this.ResumeLayout(false);
-
+            this.PerformLayout();
         }
-
-        #endregion
 
         private System.Windows.Forms.Button btnStart;
         private System.Windows.Forms.RichTextBox txtLog;
+        private System.Windows.Forms.TextBox txtMessage;
+        private System.Windows.Forms.Button btnSend;
     }
 }
