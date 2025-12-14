bd254c8ee8700f8fee17c44def198f2abf314576
tthanh1223 <lqd.tk23.vothaithanh@gmail.com>
2025-12-14 10:06:17 +0700

    sửa app để cấu trúc lại phía Client

--- ClientApp/app.py
+++ ClientApp/app.py
@@ -2,8 +2,12 @@ import uvicorn
 from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
+from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+
+# Mount static files để serve CSS và JS
+app.mount("/static", StaticFiles(directory="static"), name="static")
 
 # Khai báo thư mục chứa file HTML
 templates = Jinja2Templates(directory="templates")
@@ -16,8 +20,8 @@ async def read_root(request: Request):
     return templates.TemplateResponse("index.html", {"request": request})
 
 if __name__ == "__main__":
-    # Chạy Web Server ở cổng 3000
-    # Thay đổi: Lắng nghe trên 0.0.0.0 để cho phép truy cập từ máy khác
+    print("=" * 60)
     print("Web App đang chạy tại: http://0.0.0.0:3000")
     print("Từ máy khác, truy cập: http://<IP_CỦA_MÁY_NÀY>:3000")
-    uvicorn.run(app, host="127.0.0.1", port=3000)
+    print("=" * 60)
+    uvicorn.run(app, host="0.0.0.0", port=3000)
