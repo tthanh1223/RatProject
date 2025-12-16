32769fef11be220b226863200e1d7984115c85a8
tthanh1223 <lqd.tk23.vothaithanh@gmail.com>
2025-12-16 20:03:12 +0700

    k xài thì bỏ

--- ClientApp/static/js/utils/helpers.js
+++ ClientApp/static/js/utils/helpers.js
@@ -1,11 +1,4 @@
 // Helper functions
-export function parseQueryString() {
-    const params = new URLSearchParams(window.location.search);
-    return {
-        serverIp: params.get('server') || 'localhost'
-    };
-}
-
 export function preventSpaceKeyOnButtons() {
     document.addEventListener('keydown', function(e) {
         if (e.code === 'Space' && e.target.tagName === 'BUTTON') {
