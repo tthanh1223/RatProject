36ca81572c8b11c7213ae468030e786c1e20b9cf
tthanh1223 <lqd.tk23.vothaithanh@gmail.com>
2025-12-17 15:02:13 +0700

    them lai

--- ClientApp/static/js/utils/helpers.js
+++ ClientApp/static/js/utils/helpers.js
@@ -1,4 +1,11 @@
 // Helper functions
+export function parseQueryString() {
+    const params = new URLSearchParams(window.location.search);
+    return {
+        serverIp: params.get('server') || 'localhost'
+    };
+}
+
 export function preventSpaceKeyOnButtons() {
     document.addEventListener('keydown', function(e) {
         if (e.code === 'Space' && e.target.tagName === 'BUTTON') {
